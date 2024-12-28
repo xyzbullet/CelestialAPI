7cdfb47716fc864f64b11e00f8d84cdc10213872
Bullet <creativeteas2+bullet@gmail.com>
2024-12-28 12:29:39 -0500

    Create include.lua

---
+++ src/include.lua
@@ -0,0 +1,8 @@
+getgenv().aliases = {
+    setclipboard = getgenv().setrbxclipboard, -- Alias for setrbxclipboard
+    getdplayer = getgenv().getplayer,         -- Alias for getplayer
+    localplayer = getgenv().getlocalplayer,   -- Alias for getlocalplayer
+    listplayers = getgenv().getplayers,       -- Alias for getplayers
+    runanimation = getgenv().runanimation,         -- Alias for runanimation
+    playanimation = getgenv().playanimation        -- Alias for playanimation
+}
