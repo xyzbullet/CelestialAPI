52c0c33a8429615a13ef8840960f65a3ca596900
Bullet <creativeteas2+bullet@gmail.com>
2024-12-28 12:31:03 -0500

    Update include.lua

--- src/include.lua
+++ src/include.lua
@@ -1,8 +1,6 @@
-getgenv().aliases = {
-    setclipboard = getgenv().setrbxclipboard, -- Alias for setrbxclipboard
-    getdplayer = getgenv().getplayer,         -- Alias for getplayer
-    localplayer = getgenv().getlocalplayer,   -- Alias for getlocalplayer
-    listplayers = getgenv().getplayers,       -- Alias for getplayers
-    runanimation = getgenv().runanimation,         -- Alias for runanimation
-    playanimation = getgenv().playanimation        -- Alias for playanimation
-}
+setclipboard = getgenv().setrbxclipboard -- Alias for setrbxclipboard
+findplayer = getgenv().getplayer         -- Alias for getplayer
+localplayer = getgenv().getlocalplayer   -- Alias for getlocalplayer
+listplayers = getgenv().getplayers       -- Alias for getplayers
+runanimation = getgenv().runanimation         -- Alias for runanimation
+playanimation = getgenv().playanimation        -- Alias for playanimation
